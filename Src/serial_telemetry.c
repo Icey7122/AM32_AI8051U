9fcf0e013547d09719d8c6d0ff4ee5bf8255a51b
Qinluoyao <qinluoyao@petalmail.com>
2025-01-31 23:38:16 +0800

    change something

--- Src/serial_telemetry.c
+++ Src/serial_telemetry.c
@@ -20,19 +20,20 @@ uint8_t get_crc8(uint8_t* Buf, uint8_t BufLen)
 
 void makeTelemPackage(uint8_t temp, uint16_t voltage, uint16_t current, uint16_t consumption, uint16_t e_rpm)
 {
-    aTxBuffer[0] = temp; // temperature
 
-    aTxBuffer[1] = (voltage >> 8) & 0xFF; // voltage hB
-    aTxBuffer[2] = voltage & 0xFF; // voltage   lowB
+    aTxBuffer[0] = (voltage >> 8) & 0xFF; // voltage hB
+    aTxBuffer[1] = voltage & 0xFF; // voltage   lowB
 
-    aTxBuffer[3] = (current >> 8) & 0xFF; // current
-    aTxBuffer[4] = current & 0xFF; // divide by 10 for Amps
+    aTxBuffer[2] = (current >> 8) & 0xFF; // current
+    aTxBuffer[3] = current & 0xFF; // divide by 10 for Amps
 
-    aTxBuffer[5] = (consumption >> 8) & 0xFF; // consumption
-    aTxBuffer[6] = consumption & 0xFF; //  in mah
+    aTxBuffer[4] = (consumption >> 8) & 0xFF; // consumption
+    aTxBuffer[5] = consumption & 0xFF; //  in mah
 
-    aTxBuffer[7] = (e_rpm >> 8) & 0xFF; //
-    aTxBuffer[8] = e_rpm & 0xFF; // eRpM *100
+    aTxBuffer[6] = (e_rpm >> 8) & 0xFF; //
+    aTxBuffer[7] = e_rpm & 0xFF; // eRpM *100
+
+    aTxBuffer[8] = temp; // temperature
 
     aTxBuffer[9] = get_crc8(aTxBuffer, 9);
 }
